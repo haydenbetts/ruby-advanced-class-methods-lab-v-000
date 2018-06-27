52f6f6ea728eac29362bf4c403b1dd4d9011e9e8
Hayden Betts <haydenbetts@gmail.com>
2018-06-27 22:26:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,10 @@ class Song
     self.all.find {|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    create_by_name(name) if !(self.find_by_name(name))
+      
+    
+  end
 
 end
